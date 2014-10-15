572fc0e0ca2b06e8f975f4e5b8c5018e9bc3b1d4
User #142 <Account.156@devlan.net>
2014-10-15 16:27:31 -0400

    Removed unused files and directories bin2array* and cryptcat-c-port in server directory.

--- server/cryptcat-c-port/netcat.h
+++
@@ -1,6 +0,0 @@
-#ifndef	__NETCAT_H
-#define	__NETCAT_H
-
-int netcat_main( int argc, char **argv );
-
-#endif	//__NETCAT_H
--- server/cryptcat-c-port/port.h
+++
@@ -1,9 +0,0 @@
-#ifndef	__PORT_H
-#define __PORT_H
-
-// defines that assisted with the port from C++ to C
-typedef int	bool;
-#define false	0
-#define	true	1
-
-#endif	//__PORT_H
